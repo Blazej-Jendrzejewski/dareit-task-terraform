ce12eb51b7f8458084cc932945deb60d3401542f
Błażej Jendrzejewski <104983510+Blazej-Jendrzejewski@users.noreply.github.com>
2023-03-31 14:51:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,31 @@
+
+resource "google_compute_instance" "dareit-vm-ci" {
+  name         = "dareit-vm-tf-ci"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["dareit"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        managed_by_terraform = "true"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
+
 resource "google_storage_bucket" "blazej_new_bucket" {
   name = "blazej-new-bucket"
   location = "US"
 }
-
-
 

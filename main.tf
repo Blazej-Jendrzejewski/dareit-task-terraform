598b431df43c03265458c146a7409e6aa764dcbd
Błażej Jendrzejewski <104983510+Blazej-Jendrzejewski@users.noreply.github.com>
2023-03-31 14:34:40 +0200

    deleted unnecessary lines

--- main.tf
+++ main.tf
@@ -3,22 +3,5 @@ resource "google_storage_bucket" "blazej_new_bucket" {
   location = "US"
 }
 
-  tags = ["dareit"]
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        managed_by_terraform = "true"
-      }
-    }
-  }
 
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-}

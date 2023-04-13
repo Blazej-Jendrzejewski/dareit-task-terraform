c5cac303cbffbb2fe8d78bf6691ff2619396e4d2
Błażej Jendrzejewski <104983510+Blazej-Jendrzejewski@users.noreply.github.com>
2023-04-13 15:17:56 +0200

    Additional configuration files

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "gcp_credentials" {
+  description = "JSON string of the Google Cloud Platform credentials"
+  type        = string
+}
+
+variable "gcp_project_id" {
+  description = "Google Cloud Platform project ID"
+  type        = string
+}
